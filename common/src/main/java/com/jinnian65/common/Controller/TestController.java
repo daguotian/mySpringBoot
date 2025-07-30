2a8e01e8291bfa6745444bbfba17031e602660ec
Ola Corwin <135567910+daguotian@users.noreply.github.com>
2025-07-30 14:52:53 +0800

    Add files via upload

---
+++ common/src/main/java/com/jinnian65/common/Controller/TestController.java
@@ -0,0 +1,12 @@
+package com.jinnian65.common.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping
+    public String test() {
+        return "Hello, World!";
+    }
+}
